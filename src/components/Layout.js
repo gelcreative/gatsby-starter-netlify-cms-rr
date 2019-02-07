dc53144c9d41eb209b8020c3d2bac2c4236fb9cd
Gel Studio <devondaviau@yahoo.ca>
2019-02-07 10:46:15 -0500

    remove yarn.lock, install styled-components, move all styles into styled components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,129 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from "gatsby"
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import bulma from 'bulma'
+import initialVariables from 'bulma/sass/utilities/initial-variables.sass'
 
 import Navbar from '../components/Navbar'
-import './all.sass'
+
+const theme = {
+  sansFont: 'IBM Plex Sans',
+  serifFont: 'IBM Plex Serif'
+}
+
+const GlobalStyle = createGlobalStyle`
+  @import url(${initialVariables});
+
+  .navbar .navbar-menu {
+    box-shadow:none !important;
+  }
+
+  .content .taglist {
+    list-style: none;
+    margin-bottom: 0;
+    margin-left: 0;
+    margin-right: 1.5rem;
+    margin-top: 1.5rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: left;
+    align-items: center;
+  }
+    
+  .content .taglist li {
+    padding: 0 2rem 1rem 0;
+    margin-bottom: 1.5rem;
+    margin-top: 0;
+  }
+
+  /* Helper Classes */
+  .full-width-image-container {
+    width: 100vw;
+    height: 400px;
+    position: relative;
+    left: 50%;
+    right: 50%;
+    margin: 5em -50vw;
+    background-size: cover;
+    background-position: bottom;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .margin-top-0 {
+    margin-top: 0 !important
+  }
+
+  @import url(${bulma});
+
+  html {
+    font-size: 10px;
+    box-sizing: border-box;
+  }
+
+  body {
+    @import url("https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700|IBM+Plex+Serif:400,500");
+    padding: 0;
+    margin: 0;
+    font-size: 1.8rem;
+    line-height: 2;
+    font-family: ${theme.sansFont}, sans-serif;
+    color: #434244;
+  }
+
+  h1, h2 {
+    font-family: ${theme.serifFont}, serif;
+    font-weight: 400;
+  }
+
+  h3, h4 {
+    font-family: ${theme.sansFont}, sans-serif;
+    font-weight: 400;
+  }
+
+  h1 {
+    font-size: 3.2rem;
+  }
+
+  h2 {
+    font-size: 2.5rem;
+  }
+
+  h3 {
+    font-size: 2.5rem;
+  }
+
+  h4 {
+    font-size: 2rem;
+  }
+
+  *, *::before, *::after {
+    box-sizing: inherit;
+  }
+
+  a {
+    color: #434244;
+    transition: 300ms;
+  }
+
+  a:hover {
+    color: #f26339;
+    text-decoration: underline;
+  }
+
+  .visually-hidden {
+    position: absolute !important;
+    clip: rect(1px 1px 1px 1px);
+    clip: rect(1px, 1px, 1px, 1px);
+    padding:0 !important;
+    border:0 !important;
+    height: 1px !important;
+    width: 1px !important ;
+    overflow: hidden;
+  }
+`
 
 const TemplateWrapper = ({ children }) => (
   <StaticQuery
@@ -18,7 +138,7 @@ const TemplateWrapper = ({ children }) => (
         }
     `}
     render={data => (
-      <div>
+      <>
         <Helmet>
           <html lang="en" />
           <title>{data.site.siteMetadata.title}</title>
@@ -36,11 +156,18 @@ const TemplateWrapper = ({ children }) => (
           <meta property="og:url" content="/" />
           <meta property="og:image" content="/img/og-image.jpg" />
         </Helmet>
+        <GlobalStyle />
         <Navbar />
         <div>{children}</div>
-      </div>
+      </>
     )}
   />
 )
 
-export default TemplateWrapper
+const WrappedWithThemeProvider = ({ children }) => (
+  <ThemeProvider theme={ theme }>
+    <TemplateWrapper children={ children } />
+  </ThemeProvider>
+)
+
+export default WrappedWithThemeProvider

b445ea138d2777f6fabf8c651df8112db4ca9f50
Gel Studio <devondaviau@yahoo.ca>
2019-02-07 16:56:58 -0500

    move theme and GlobalStyle into their own file, style and structure for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,81 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'gatsby'
+
+import squareLogo from '../img/retirerite-logo-square.svg'
 
 const StyledFooter = styled.footer`
-  p {
-    color: chartreuse;
+  border-top: 5px solid ${props => props.theme.red};
+  background-color: ${props => props.theme.lightGrey};
+
+  .retirerite-footer-top {
+    padding: 4em 0 5em 0;
+  }
+
+  .retirerite-footer-navigation-outer h2 {
+    font-family: ${props => props.theme.sansFont}, sans-serif;
+    text-decoration: underline;
+  }
+
+  .retirerite-footer-navigation {
+    text-align: left;
+  }
+
+  .retirerite-footer-bottom {
+    background-color: ${props => props.theme.darkGrey};
+    padding: 2em 0;
   }
 `
 
-const Footer = () => {
-  return (
-    <p>Hello there.</p>
-  )
-}
+const Footer = () => (
+  <StyledFooter>
+    <section class="container retirerite-footer-top">
+      <div class="columns">
+        <div class="column is-one-third">
+          <div class="columns">
+            <div class="column is-one-third">
+              <Link to="/">
+                <img src={squareLogo} title="RetireRite Logo" alt="RetireRite Logo" />
+              </Link>
+            </div>
+            <div class="column is-two-thirds">
+              <p>RetireRite Financial Strategies Inc.</p>
+              <address>
+                Suite 307 - 126 Wellington Street West<br />
+                Barrie, ON<br />
+                L4N 1K9
+              </address>
+            </div>
+          </div>
+        </div>
+        <div class="column is-two-thirds retirerite-footer-navigation-outer">
+          <div class="columns is-centered">
+            <div class="column is-narrow">
+              <h2>Site Navigation</h2>
+              <ul class="retirerite-footer-navigation">
+                <li>About</li>
+                <li>Services</li>
+                <li>Blog</li>
+                <li>FAQ</li>
+                <li>Resources</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="retirerite-footer-bottom">
+      <div class="container">
+        <div class="columns">
+          <div class="column">&copy; { new Date().getFullYear() } RetireRite Strategies Inc.</div>
+          <div class="column">
+            <h2>Social Media</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  </StyledFooter>
+)
 
 export default Footer
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,132 @@
+import { createGlobalStyle } from 'styled-components'
+import bulma from 'bulma'
+import initialVariables from 'bulma/sass/utilities/initial-variables.sass'
+
+const theme = {
+  sansFont: 'IBM Plex Sans',
+  serifFont: 'IBM Plex Serif',
+  red: '#ef4136',
+  blue: '#262261',
+  darkBlue: '#28313e',
+  lightGrey: '#f3f3f3',
+  darkGrey: '#979aa1',
+}
+
+const GlobalStyle = createGlobalStyle`
+  @import url(${initialVariables});
+
+  .navbar .navbar-menu {
+    box-shadow:none !important;
+  }
+
+  .content .taglist {
+    list-style: none;
+    margin-bottom: 0;
+    margin-left: 0;
+    margin-right: 1.5rem;
+    margin-top: 1.5rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: left;
+    align-items: center;
+  }
+    
+  .content .taglist li {
+    padding: 0 2rem 1rem 0;
+    margin-bottom: 1.5rem;
+    margin-top: 0;
+  }
+
+  /* Helper Classes */
+  .full-width-image-container {
+    width: 100vw;
+    height: 400px;
+    position: relative;
+    left: 50%;
+    right: 50%;
+    margin: 5em -50vw;
+    background-size: cover;
+    background-position: bottom;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .margin-top-0 {
+    margin-top: 0 !important
+  }
+
+  @import url(${bulma});
+
+  html {
+    font-size: 10px;
+    box-sizing: border-box;
+  }
+
+  body {
+    @import url("https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700|IBM+Plex+Serif:400,500");
+    padding: 0;
+    margin: 0;
+    font-size: 1.8rem;
+    line-height: 2;
+    font-family: ${theme.sansFont}, sans-serif;
+    color: #434244;
+  }
+
+  h1, h2 {
+    font-family: ${theme.serifFont}, serif;
+    font-weight: 400;
+  }
+
+  h3, h4 {
+    font-family: ${theme.sansFont}, sans-serif;
+    font-weight: 400;
+  }
+
+  h1 {
+    font-size: 3.2rem;
+  }
+
+  h2 {
+    font-size: 2.5rem;
+  }
+
+  h3 {
+    font-size: 2.5rem;
+  }
+
+  h4 {
+    font-size: 2rem;
+  }
+
+  *, *::before, *::after {
+    box-sizing: inherit;
+  }
+
+  a {
+    color: #434244;
+    transition: 300ms;
+  }
+
+  a:hover {
+    color: #f26339;
+    text-decoration: underline;
+  }
+
+  address {
+    font-style: normal;
+  }
+
+  .visually-hidden {
+    position: absolute !important;
+    clip: rect(1px 1px 1px 1px);
+    clip: rect(1px, 1px, 1px, 1px);
+    padding:0 !important;
+    border:0 !important;
+    height: 1px !important;
+    width: 1px !important ;
+    overflow: hidden;
+  }
+`
+
+export { theme, GlobalStyle }

14231f4c3b7a0132a0d4f2615f503a2c3f996dfa
Gel Studio <devondaviau@yahoo.ca>
2019-02-08 12:06:06 -0500

    navbar restructure and style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,80 +2,119 @@ import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
 
-import github from '../img/github-icon.svg'
 import logo from '../img/retirerite-logo-hort.svg'
+
+const StyledHeader = styled.nav`
+  .navbar-brand {
+    padding: 2em 0;
+  }
+
+  .navbar-brand img {
+    width: 250px;
+  }
+
+  .retirerite-nav-bottom {
+    background-color: #2e3440;
+  }
+
+  .retirerite-nav-bottom a {
+    color: #ffffff;
+    font-weight: 700;
+    height: 60px;
+  }
+
+  .retirerite-navbar-start [aria-current] {
+    position: relative;
+  }
+
+  .retirerite-navbar-start [aria-current]:after {
+      content: '';
+      width: 0; 
+      height: 0; 
+      border-left: 7px solid transparent;
+      border-right: 7px solid transparent;
+      border-bottom: 12px solid #ffffff;
+      position: absolute;
+      bottom: 0;
+      left: 50%;
+      transform: translateX(-50%);
+  }
+
+  .retirerite-navbar-start {
+    display: flex;
+    justify-content: space-around;
+  }
+`
 
 const Navbar = class extends React.Component {
 
   componentDidMount() {
     // Get all "navbar-burger" elements
-   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
     // Check if there are any navbar burgers
-   if ($navbarBurgers.length > 0) {
- 
-     // Add a click event on each of them
-     $navbarBurgers.forEach( el => {
-       el.addEventListener('click', () => {
- 
-         // Get the target from the "data-target" attribute
-         const target = el.dataset.target;
-         const $target = document.getElementById(target);
- 
-         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-         el.classList.toggle('is-active');
-         $target.classList.toggle('is-active');
- 
-       });
-     });
-   }
- }
- 
- render() {
-   return (
+    if ($navbarBurgers.length > 0) {
+
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+
+        });
+      });
+    }
+  }
+
+  render() {
+    return (
   
-  <nav className="navbar is-transparent" role="navigation" aria-label="main-navigation">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-        </Link>
-        {/* Hamburger menu */}
-        <div className="navbar-burger burger" data-target="navMenu">
-          <span></span>
-          <span></span>
-          <span></span>
+    <StyledHeader className="is-transparent" role="navigation" aria-label="main-navigation">
+      <div className="container">
+        <div className="navbar-brand">
+          <Link to="/" title="Logo">
+            <img src={logo} alt="RetireRite"/>
+          </Link>
         </div>
       </div>
-      <div id="navMenu" className="navbar-menu">
-      <div className="navbar-start has-text-centered">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link>
+      <div className="retirerite-nav-bottom">
+        <div className="container">
+          <div id="navMenu" className="retirerite-navbar-menu">
+            <div className="retirerite-navbar-start has-text-centered">
+              <Link className="navbar-item" to="/about">
+                About
+              </Link>
+              <Link className="navbar-item" to="/products">
+                Services
+              </Link>
+              <Link className="navbar-item" to="/blog">
+                Blog
+              </Link>
+              <Link className="navbar-item" to="/faq">
+                FAQ
+              </Link>
+              <Link className="navbar-item" to="/contact/examples">
+                Tools &amp; Resources
+              </Link>
+              <Link className="navbar-item" to="/contact">
+                Contact
+              </Link>
+            </div>
+          </div>
+          {/* Hamburger menu */}
+          <div className="navbar-burger burger" data-target="navMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
       </div>
-      <div className="navbar-end has-text-centered">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
-      </div>
-    </div>
-  </nav>
+    </StyledHeader>
   )}
 }
 

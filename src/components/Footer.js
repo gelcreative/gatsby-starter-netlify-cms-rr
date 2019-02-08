ddf6c747418b6c9ab2186d6696d807bb7537b5a8
Gel Studio <devondaviau@yahoo.ca>
2019-02-08 11:14:41 -0500

    bring in RetireRite logo, footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,15 @@ const StyledFooter = styled.footer`
   background-color: ${props => props.theme.lightGrey};
 
   .retirerite-footer-top {
-    padding: 4em 0 5em 0;
+    padding: 4em 5% 5em;
   }
 
-  .retirerite-footer-top img[title="RetireRite Logo"] {
-    max-width: 150px;
+  .retirerite-gray-border-right {
+    border-right: 2px solid #cacaca;
+  }
+
+  .retirerite-footer-logo-column img {
+    max-width: 120px;
   }
 
   .retirerite-footer-navigation-outer h2 {
@@ -47,7 +51,12 @@ const StyledFooter = styled.footer`
       height: 30px;
   }
 
-  @media(max-width: 600px) {
+  .retirerite-twitter img {
+    height: 27px;
+  }
+
+
+  @media(max-width: 769px) {
       .retirerite-footer-bottom .column {
           justify-content: center;
           text-align: center;
@@ -59,9 +68,9 @@ const Footer = () => (
   <StyledFooter>
     <section className="container retirerite-footer-top">
       <div className="columns">
-        <div className="column is-one-third">
+        <div className="column is-5-desktop is-half-tablet retirerite-gray-border-right">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-one-third retirerite-footer-logo-column">
               <Link to="/">
                 <img src={squareLogo} title="RetireRite Logo" alt="RetireRite Logo" />
               </Link>
@@ -73,21 +82,26 @@ const Footer = () => (
                 Barrie, ON<br />
                 L4N 1K9
               </address>
+              <p><a href="tel:+17057285551">Office: (705) 728-5551</a></p>
+              <p>Fax: (705) 728-0012</p>
+              <p><a href="tel:+18002878844">Toll Free: 1-800-287-8844</a></p>
             </div>
           </div>
         </div>
-        <div className="column is-two-thirds retirerite-footer-navigation-outer">
+        <div className="column is-7-desktop is-half-tablet retirerite-footer-navigation-outer">
           <div className="columns is-centered">
             <div className="column is-narrow">
               <h2>Site Navigation</h2>
-              <ul className="retirerite-footer-navigation">
-                <li>About</li>
-                <li>Services</li>
-                <li>Blog</li>
-                <li>FAQ</li>
-                <li>Resources</li>
-                <li>Contact</li>
-              </ul>
+              <nav role="navigation">
+                <ul className="retirerite-footer-navigation">
+                  <li>About</li>
+                  <li>Services</li>
+                  <li>Blog</li>
+                  <li>FAQ</li>
+                  <li>Resources</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
             </div>
           </div>
         </div>
@@ -100,10 +114,10 @@ const Footer = () => (
             <p>
               &copy; { new Date().getFullYear() } RetireRite Strategies Inc.
             </p>
-            </div>
+          </div>
           <div className="column retirerite-footer-social">
             <ul className="retirerite-footer-social">
-              <li>
+              <li className="retirerite-twitter">
                 <a href="https://twitter.com/retirerite_inc" title="RetireRite on Twitter" target="_blank" rel="noopener noreferrer">
                   <img src={twitter} alt="Twitter" />
                 </a>

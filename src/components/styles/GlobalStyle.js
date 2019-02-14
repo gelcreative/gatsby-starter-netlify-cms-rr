9e24d971fdc0430e7cefc670c43642693a8ec92c
Gel Studio <devondaviau@yahoo.ca>
2019-02-14 16:22:58 -0500

    remove some whitespace

--- src/components/styles/GlobalStyle.js
+++ src/components/styles/GlobalStyle.js
@@ -106,16 +106,15 @@ const GlobalStyle = createGlobalStyle`
   }
 
   .visually-hidden {
-        position: absolute !important;
-        clip: rect(1px 1px 1px 1px);
-        clip: rect(1px, 1px, 1px, 1px);
-        padding:0 !important;
-        border:0 !important;
-        height: 1px !important;
-        width: 1px !important ;
-        overflow: hidden;
-      }
-
+    position: absolute !important;
+    clip: rect(1px 1px 1px 1px);
+    clip: rect(1px, 1px, 1px, 1px);
+    padding:0 !important;
+    border:0 !important;
+    height: 1px !important;
+    width: 1px !important ;
+    overflow: hidden;
+  }
 `
 
 export { theme, GlobalStyle }

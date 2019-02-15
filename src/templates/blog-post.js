7e39a22ee7007a36b38f2761c84bcdb067b62e57
Gel Studio <devondaviau@yahoo.ca>
2019-02-15 16:44:19 -0500

    start building out the template and styles

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,8 +3,19 @@ import PropTypes from 'prop-types'
 import { kebabCase } from 'lodash'
 import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
+import styled from 'styled-components'
+
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+
+const StyledBlogPost = styled.article`
+  background: #ffff;
+  .retire-rite-blog-masthead {
+    height: 300px;
+    background: center no-repeat;
+    background-size: cover;
+  }
+`
 
 export const BlogPostTemplate = ({
   content,
@@ -12,14 +23,25 @@ export const BlogPostTemplate = ({
   description,
   tags,
   title,
+  bannerImage,
   helmet,
 }) => {
   const PostContent = contentComponent || Content
 
   return (
-    <section className="section">
+    <StyledBlogPost className="section">
       {helmet || ''}
-      <div className="container content">
+      <div 
+        className="retire-rite-blog-masthead"
+        style={{
+          backgroundImage: `url(${
+            !!bannerImage.childImageSharp 
+            ? bannerImage.childImageSharp.fluid.src 
+            : bannerImage
+          })`
+        }}
+      ></div>
+      <section className="container content">
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
@@ -41,8 +63,8 @@ export const BlogPostTemplate = ({
             ) : null}
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </StyledBlogPost>
   )
 }
 
@@ -73,6 +95,7 @@ const BlogPost = ({ data }) => {
         }
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
+        bannerImage={post.frontmatter.bannerImage.image}
       />
     </Layout>
   )
@@ -95,6 +118,15 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         title
         description
+        bannerImage {
+              image {
+                childImageSharp {
+                  fluid(maxWidth: 2048, quality: 100) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
         tags
       }
     }

4e15560f608bc3ec14694c90f1d32cceb42c2673
Gel Studio <devondaviau@yahoo.ca>
2019-02-26 13:43:05 -0500

    start styling out component

--- src/components/EmployeeProfile.js
+++ src/components/EmployeeProfile.js
@@ -3,7 +3,29 @@ import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 
 const StyledEmployee = styled.article`
-  background: #ffffff;
+  display: flex;
+
+  .retirerite-employee-intro {
+    min-width: 25%;
+    flex-grow: 1;
+    .retirerite-employee-name {
+      font-size: 2.2rem;
+      .retirerite-employee-credentials {
+        font-size: 1.2rem;
+        font-style: italic;
+        color: ${props => props.theme.darkGrey};
+        display: flex;
+        border-top: 1px solid ${props => props.theme.middleBlue};
+        margin-top: 0.5em;
+        padding-top: 0.25em;
+      }
+    }
+  }
+
+  .retirerite-employee-bio {
+    margin: 1em;
+    flex-grow: 2;
+  }
 `
 
 const EmployeeProfile = () => (
@@ -28,15 +50,17 @@ const EmployeeProfile = () => (
     render={data => {
       const employees = data.allEmployeesYaml.edges
       return (
-        <StyledEmployee>
+        <>
           {employees.map(({ node: employee }) => (
-            <>
-              <img src={employee.profileImage.image} alt={employee.profileImage.alt} />
-              <p className="retirerite-employee-name">{employee.name}<span className="visually-hidden">, </span><span className="retirerite-employee-credentials">{employee.credentials}</span></p>
-              <p className="retirerite-employee-bio">{employee.bio}</p>
-            </>
+            <StyledEmployee>
+              <div className="retirerite-employee-intro">
+                  <img src={employee.profileImage.image} alt={employee.profileImage.alt} />
+                  <p className="retirerite-employee-name">{employee.name}<span className="visually-hidden">, </span><span className="retirerite-employee-credentials">{employee.credentials}</span></p>
+                </div>
+                <p className="retirerite-employee-bio">{employee.bio}</p>
+            </StyledEmployee>
           ))}
-        </StyledEmployee>
+        </>
       )
     }}
   />

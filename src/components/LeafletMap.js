77b381e336d86b3892c528a9b2205c60b0d02df9
Gel Studio <devondaviau@yahoo.ca>
2019-02-26 17:10:38 -0500

    install react-leaflet and refactor map component

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react'
-import Leaflet from 'leaflet'
+import { Map, Marker, TileLayer } from 'react-leaflet'
 
 class LeafletMap extends Component {
-  initLeafletMap = () => {
-    var retireriteMap = Leaflet.map('retirerite-map').setView([44.390092, -79.704469], 16);
+  // initLeafletMap = () => {
+  //   var retireriteMap = Leaflet.map('retirerite-map').setView([44.390092, -79.704469], 16);
 
-    Leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox.streets',
-      accessToken: 'pk.eyJ1IjoiZ2VsYWdlbmN5IiwiYSI6ImNqc2YyMHdvNjAyeXM0NHFwanl5cDdwbzQifQ.DBHeFeHLQ9cMeJXBs1-R1w'
-    }).addTo(retireriteMap);
+  //   Leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  //     maxZoom: 18,
+  //     id: 'mapbox.streets',
+  //     accessToken: 'pk.eyJ1IjoiZ2VsYWdlbmN5IiwiYSI6ImNqc2YyMHdvNjAyeXM0NHFwanl5cDdwbzQifQ.DBHeFeHLQ9cMeJXBs1-R1w'
+  //   }).addTo(retireriteMap);
 
-    Leaflet.marker([44.390092, -79.704469]).addTo(retireriteMap);
-  }
-
-  componentDidMount() {
-    this.initLeafletMap()
-  }
+  //   Leaflet.marker([44.390092, -79.704469]).addTo(retireriteMap);
+  // }
 
   render() {
-    return <div id="retirerite-map" style={{ height: '300px' }}></div>
+    const position = [44.390092, -79.704469]
+    const accessToken = 'pk.eyJ1IjoiZ2VsYWdlbmN5IiwiYSI6ImNqc2YyMHdvNjAyeXM0NHFwanl5cDdwbzQifQ.DBHeFeHLQ9cMeJXBs1-R1w'
+    return (
+      <Map
+        center={position}
+        zoom={16}
+        maxZoom={18}
+      >
+        <TileLayer
+          id='mapbox.streets'
+          accessToken={accessToken}
+          url='https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+        >
+          <Marker
+            position={position}
+          >
+          </Marker>
+        </TileLayer>
+      </Map>
+    )
   }
 }
 

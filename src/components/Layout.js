082027e4e5e26cf5e2fe7828b4e337d540d1f014
Gel Studio <devondaviau@yahoo.ca>
2019-02-28 10:22:19 -0500

    verify map styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -41,6 +41,7 @@ const TemplateWrapper = ({ children }) => (
 
           {/* Leaflet CSS */}
           <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="crossorigin=""/>
+
         </Helmet>
         <GlobalStyle />
         <Navbar />
--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -2,18 +2,6 @@ import React, { Component } from 'react'
 import { Map, Marker, TileLayer } from 'react-leaflet'
 
 class LeafletMap extends Component {
-  // initLeafletMap = () => {
-  //   var retireriteMap = Leaflet.map('retirerite-map').setView([44.390092, -79.704469], 16);
-
-  //   Leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  //     maxZoom: 18,
-  //     id: 'mapbox.streets',
-  //     accessToken: 'pk.eyJ1IjoiZ2VsYWdlbmN5IiwiYSI6ImNqc2YyMHdvNjAyeXM0NHFwanl5cDdwbzQifQ.DBHeFeHLQ9cMeJXBs1-R1w'
-  //   }).addTo(retireriteMap);
-
-  //   Leaflet.marker([44.390092, -79.704469]).addTo(retireriteMap);
-  // }
 
   render() {
     const position = [44.390092, -79.704469]
@@ -23,6 +11,7 @@ class LeafletMap extends Component {
         center={position}
         zoom={16}
         maxZoom={18}
+        style={{ height: '300px' }}
       >
         <TileLayer
           id='mapbox.streets'

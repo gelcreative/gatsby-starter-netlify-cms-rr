20674d03a1250096ec2ff8db85c15f6f5c556517
Gel Studio <devondaviau@yahoo.ca>
2019-03-01 12:02:56 -0500

    scaffold component

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,2 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
+
+import SingleBlogPost from './SingleBlogPost'
+
+const StyledBlogList = styled.section`
+  background: #ffffff;
+`
+
+const BlogList = () => {
+  return ()
+}
+
+export default BlogList

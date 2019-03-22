1ec68fb4636188d2431ebfe60f0833000ab0b2dc
Gel Studio <devondaviau@yahoo.ca>
2019-03-22 11:29:09 -0400

    edit site title and meta description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'RetireRite Financial Strategies Inc.',
+    description: 'RetireRite is your trusted financial strategist, with decades of experience.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',

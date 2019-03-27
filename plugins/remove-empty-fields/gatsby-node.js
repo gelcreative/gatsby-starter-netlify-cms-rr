26bb8ae1aab025d995c15e5312b5ca66e0b6574f
Gel Studio <devondaviau@yahoo.ca>
2019-03-27 13:53:04 -0400

    add some comments and attribution

--- plugins/remove-empty-fields/gatsby-node.js
+++ plugins/remove-empty-fields/gatsby-node.js
@@ -1,12 +1,18 @@
+/* Found this at:
+* https://stackoverflow.com/questions/54292012/handle-empty-strings-as-image-paths-when-using-gatsby-transformer-sharp?rq=1
+*/
+
 let fieldsToRemove = []
 
 const deleteFieldsRecursive = (node) => {
+  // if node is an empty string, delete it
   fieldsToRemove.forEach(fieldToRemove => {
     if (node[fieldToRemove] === '') {
       delete node[fieldToRemove]
     }
   })
 
+  // if node is an empty object, go into it and evaluate subnodes
   if (typeof node === 'object') {
     Object.values(node).forEach(subNode => {
       deleteFieldsRecursive(subNode)
@@ -17,6 +23,8 @@ const deleteFieldsRecursive = (node) => {
 exports.onCreateNode = ({ node }, configOptions) => {
   fieldsToRemove = configOptions.fieldsToRemove
 
+  // if node is a markdownremark node, but doesn't have
+  // frontmatter, then return
   if (node.internal.type === 'MarkdownRemark') {
     if (!node.frontmatter) {
       return;
